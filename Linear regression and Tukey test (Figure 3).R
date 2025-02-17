945b41696dc83caffe125160f644a18bea5b4618
Megan Bushnell <mebushnell@outlook.com>
2025-02-17 18:32:21 +0000

    Add files via upload

---
+++ Linear regression and Tukey test (Figure 3).R
@@ -0,0 +1,24 @@
+#Set your working directory.
+setwd("G:/Ch 7 2025-02-03")
+
+#Load in the packages. 
+library(ggplot2)
+library(multcomp)
+library(plyr)
+
+#read in the data
+book<- read.csv("Figure 3 data.csv")
+
+#Creating linear model that tests effect of Book, Pragmatic typ, and Mortality 
+#on Line ratios.
+lm<-lm(Number.of.Latin.lines ~ Book, book)
+
+#Analysis of variance on linear model; produces p-values to indicate which 
+#variable is significant.
+av<-aov(lm)
+summary(av)
+
+#Tukey test; indicates where significant differences lie between variable 
+#values.
+tukey<-TukeyHSD(av)
+tukey
